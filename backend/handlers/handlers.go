179429c208363bfc73f7be15076a2cfcf8b93b15
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-07-01 23:53:23 +0900

    Refactor handlers in handlers.go to add named functions for article posting, listing, detail retrieval, and posting comments, enhancing code clarity and organization.

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -9,24 +10,24 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello world\n")
 }
 
-func(w http.ResponseWriter, req *http.Request) {
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Article...\n")
 }
 
-func(w http.ResponseWriter, req *http.Request) {
+func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Article List\n")
 }
 
-func(w http.ResponseWriter, req *http.Request) {
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	articleID := 1
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
 	io.WriteString(w, resString)
 }
 
-func(w http.ResponseWriter, req *http.Request) {
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Nice...\n")
 }
 
-func(w http.ResponseWriter, req *http.Request) {
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Comment...\n")
 }
