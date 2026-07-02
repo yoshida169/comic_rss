0d5872c6eebef2e672ff53ad5410422860940463
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-07-02 23:34:09 +0900

    Add ForList component to display a list of books in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
 import MyHello from './MyHello'
 import StateBasic from './StateBasic'
+import books from './Books'
+import ForList from './ForList'
 
 function App() {
   return (
+    <ForList src={books} />
     <div className="App">
       <MyHello myName="太郎" />
       <button type="submit" onClick={() => getComics()}>
