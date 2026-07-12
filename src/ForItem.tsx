8b3c2ff38ca131d6005df5841050651b7c46a28a
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-07-12 22:36:43 +0900

    Refactor ForItem component: streamline conditional rendering of download link using an inline function for improved readability and maintainability.

--- src/ForItem.tsx
+++ src/ForItem.tsx
@@ -2,20 +2,20 @@ import type { Book } from './Books'
 import Download from './Download';
 
 export default function ForItem({ book }: { book: Book }) {
-  let dd;
-  if (book.download){
-    dd = <dd>{book.summary}<Download isbn={book.isbn} /></dd>
-  } else {
-    dd = <dd>{book.summary}</dd>
-  }
   return (
-    <>
-      <dt>
-        <a href={`https://wings.web-deli.com/books/${book.isbn}.jpg`}>
-          {book.title} ({book.price}円)
-        </a>
-      </dt>
-      {dd}
-    </>
+  <>
+    <dt>
+    <a href={`https://wings.web-deli.com/books/${book.isbn}.jpg`}>
+      {book.title} ({book.price}円)
+    </a>
+    </dt>
+    {(() => {
+      if (book.download) {
+        return <dd>{book.summary}<Download isbn={book.isbn} /></dd>
+      } else {
+        return <dd>{book.summary}</dd>
+      }
+    })()}
+  </>
   )
 }
