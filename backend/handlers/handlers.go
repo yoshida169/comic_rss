6ef7e99a5161b304a909232fb97521229b4090d5
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-07-16 23:01:29 +0900

    Implement method validation in handlers: ensure HTTP methods are checked for PostArticleHandler, ArticleDetailHandler, ArticleListHandler, PostNiceHandler, and PostCommentHandler, returning appropriate errors for invalid methods.

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -15,23 +15,43 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Article...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Article...\n")
+	} else {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID := 1
-	resString := fmt.Sprintf("Article No.%d\n", articleID)
-	io.WriteString(w, resString)
+	if req.Method == http.MethodGet {
+		articleID := 1
+		resString := fmt.Sprintf("Article No.%d\n", articleID)
+		io.WriteString(w, resString)
+	} else {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Article List\n")
+	} else {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Nice...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Nice...\n")
+	} else {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Comment...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Comment...\n")
+	} else {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
