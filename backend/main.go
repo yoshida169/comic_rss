1eb3b77440845ee303c66c60817e189d6a2c415a
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-07-19 00:28:17 +0900

    Implement gorilla/mux for routing in main.go, enhancing HTTP request handling and organization of route definitions.

--- backend/main.go
+++ backend/main.go
@@ -4,16 +4,20 @@ import (
 	"comic-rss-backend/handlers"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ArticleListHandler)
-	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handlers.HelloHandler)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	r.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
