d436aa5fd5f74cabe36919a1cf2497fc0656b338
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-07-21 23:41:52 +0900

    Update ArticleDetailHandler to parse article ID from URL and handle invalid parameters
    - Modified ArticleDetailHandler to retrieve the article ID from the request URL using gorilla/mux.
    - Added error handling for invalid article ID parameters, returning a 400 Bad Request response when necessary.

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
@@ -19,7 +22,11 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID := 1
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
+	}
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
 	io.WriteString(w, resString)
 }
