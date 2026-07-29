9cc59550a56249fab94bb2ec638fc427d7ee6faf
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-07-29 22:06:56 +0900

    Update JSON tags in Comment and Article structs in main.go
    - Added JSON tags to fields in Comment and Article structs for improved serialization.
    - Corrected a typo in the Printf function for displaying JSON data.

--- backend/main.go
+++ backend/main.go
@@ -24,20 +24,20 @@ func newRouter() http.Handler {
 }
 
 type Comment struct {
-	CommentID int
-	ArticleID int
-	Message   string
-	CreatedAt time.Time
+	CommentID int       `json:"comment_id"`
+	ArticleID int       `json:"article_id`
+	Message   string    `json:"message`
+	CreatedAt time.Time `json:"created_at`
 }
 
 type Article struct {
-	ID          int
-	Title       string
-	Contents    string
-	UserName    string
-	NiceNum     int
-	CommentList []Comment
-	CreatedAt   time.Time
+	ID          int       `json:"article_id`
+	Title       string    `json:"title`
+	Contents    string    `json:"contents`
+	UserName    string    `json:"user_name`
+	NiceNum     int       `json:"nice`
+	CommentList []Comment `json:"comments`
+	CreatedAt   time.Time `json:"created_at`
 }
 
 func main() {
@@ -72,5 +72,5 @@ func main() {
 		return
 	}
 
-	fmt.Printlf("%s\n", jsonData)
+	fmt.Printf("%s\n", jsonData)
 }
