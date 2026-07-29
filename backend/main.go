4d845aa5171d7613fa0e29ea48fa9917483549d6
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-07-29 22:11:06 +0900

    Fix JSON tags in Comment and Article structs in main.go
    - Corrected JSON tag syntax for fields in Comment and Article structs to ensure proper serialization.
    - This update enhances the data model's integrity and serialization capabilities.

--- backend/main.go
+++ backend/main.go
@@ -25,19 +25,19 @@ func newRouter() http.Handler {
 
 type Comment struct {
 	CommentID int       `json:"comment_id"`
-	ArticleID int       `json:"article_id`
-	Message   string    `json:"message`
-	CreatedAt time.Time `json:"created_at`
+	ArticleID int       `json:"article_id"`
+	Message   string    `json:"message"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type Article struct {
-	ID          int       `json:"article_id`
-	Title       string    `json:"title`
-	Contents    string    `json:"contents`
-	UserName    string    `json:"user_name`
-	NiceNum     int       `json:"nice`
-	CommentList []Comment `json:"comments`
-	CreatedAt   time.Time `json:"created_at`
+	ID          int       `json:"article_id"`
+	Title       string    `json:"title"`
+	Contents    string    `json:"contents"`
+	UserName    string    `json:"user_name"`
+	NiceNum     int       `json:"nice"`
+	CommentList []Comment `json:"comments"`
+	CreatedAt   time.Time `json:"created_at"`
 }
 
 func main() {
