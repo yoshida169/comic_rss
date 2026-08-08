0356e8b99ca890ff07c32dc9da094594d1b59ff4
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-08-09 00:43:08 +0900

    Refactor PostArticleHandler to simplify request body processing
    - Removed commented-out code related to content length handling and manual request body reading.
    - Streamlined the JSON decoding process by directly using json.NewDecoder, enhancing code clarity and reliability.
    - This update improves the overall structure of the PostArticleHandler, making it more efficient and easier to maintain.

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -17,20 +17,6 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 
 // POST /article
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	// length, err := strconv.Atoi(req.Header.Get("Content-Length"))
-	// if err != nil {
-	// 	http.Error(w, "cannot get content length\n", http.StatusBadRequest)
-	// 	return
-	// }
-	// reqBodybuffer := make([]byte, length)
-
-	// if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
-	// 	http.Error(w, "fail to encode json\n", http.StatusInternalServerError)
-	// 	return
-	// }
-
-	// defer req.Body.Close()
-
 	var reqArticle models.Article
 	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
