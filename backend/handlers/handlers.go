a3aab547f1b29a4863cae444298a3ed586aae7d2
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2026-08-09 22:39:38 +0900

    Refactor ArticleDetailHandler and ArticleListHandler for improved clarity and functionality
    - Moved the ArticleDetailHandler to the end of the file for better organization and readability.
    - Updated ArticleListHandler to use json.NewDecoder for encoding the article list, simplifying the code and enhancing performance.
    - Removed commented-out code to clean up the handlers, improving overall code maintainability.

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -28,25 +28,6 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(article)
 }
 
-// GET /article/:id
-func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
-	if err != nil {
-		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
-		return
-	}
-
-	article := models.Article1
-	jsonData, err := json.Marshal(article)
-
-	if err != nil {
-		errMsg := fmt.Sprintf("fail to encode json (articleID %d)\n", articleID)
-		http.Error(w, errMsg, http.StatusInternalServerError)
-	}
-
-	w.Write(jsonData)
-}
-
 // GET /article/list
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	queryMap := req.URL.Query()
@@ -64,11 +45,31 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	}
 
 	articleList := []models.Article{models.Article1, models.Article2}
-	jsonData, err := json.Marshal(articleList)
+	json.NewDecoder(w).Encode(articleList)
+	// jsonData, err := json.Marshal(articleList)
+	// if err != nil {
+	// 	errMsg := fmt.Sprintf("fail to encode json (page %d)\n", page)
+	// 	http.Error(w, errMsg, http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// w.Write(jsonData)
+}
+
+// GET /article/:id
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 	if err != nil {
-		errMsg := fmt.Sprintf("fail to encode json (page %d)\n", page)
-		http.Error(w, errMsg, http.StatusInternalServerError)
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
 		return
+	}
+
+	article := models.Article1
+	jsonData, err := json.Marshal(article)
+
+	if err != nil {
+		errMsg := fmt.Sprintf("fail to encode json (articleID %d)\n", articleID)
+		http.Error(w, errMsg, http.StatusInternalServerError)
 	}
 
 	w.Write(jsonData)
